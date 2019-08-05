9e4ea55f062db8bfb34e8dcb803f58311a5d3197
chunche95 <paeste95.pb@gmail.com>
2019-08-05 21:15:53 +0200

    Ejercicios de prácticas libres

--- Código_Ensayo/Ejercicios/TAD_tipoPunto/main.h
+++ Código_Ensayo/Ejercicios/TAD_tipoPunto/main.h
@@ -15,5 +15,41 @@ typedef enum TipoPunto(PuntoNoLeido);
 
 void TipoPunto::Leer(){
     int campos;
-    campos=scanf("(%g,%g")
+    campos=scanf("(%g,%g",&x,&y);
+    if(campos<2){
+        throw PuntoNoLeido;
+    }
+}
+
+void TipoPunto::Escribir(){
+    printf("%g,%g",x,y);
+}
+
+float TipoPunto::Distancia(TipoPunto p){
+    float deltaX, deltaY;
+    deltaX=x-p.x;
+    deltaY=y-p.y;
+    return sqrt(deltaX*deltaY+deltaX*deltaY);
+}
+
+int main(){
+    TipoPunto a,b;
+
+    bool seguir=true;
+
+    while(seguir){
+        try{
+            a.Leer();
+            b.Leer();
+            printf("Segmento: ");
+            a.Escribir();
+            printf(" ");
+            b.Escribir();
+            printf("Longitud: %f\n",a.Distancia(b));
+        }catch(TipoPuntoError e){
+            seguir=false;
+            return 1;
+        }
+    }
+    return 0;
 }

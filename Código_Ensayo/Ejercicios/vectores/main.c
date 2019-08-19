1239feab284704b954d053ee950adbd4e33ebbd7
chunche95 <paeste95.pb@gmail.com>
2019-08-19 18:47:18 +0200

    Vectores

---
+++ Código_Ensayo/Ejercicios/vectores/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int vector[5]; /* ={1,5,54,0,998}; */
+    int i;
+
+    for (int i=0;i<5;i++){
+        printf("Escriba un número: ");
+        scanf("%d", &vector[i]);
+    }
+    for(i=0;i<5;i++){
+        if(vector[i]==998){
+            printf("\n Posición de memoria del vector con el número 998 es: %d",i);
+        }
+    }
+    return 0;
+}

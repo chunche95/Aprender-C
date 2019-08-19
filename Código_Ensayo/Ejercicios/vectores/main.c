a83e767b4741e87d529d3e9218a0c4e3ed04e340
chunche95 <paeste95.pb@gmail.com>
2019-08-19 18:50:25 +0200

    Lista de ejercicios realizados

--- Código_Ensayo/Ejercicios/vectores/main.c
+++ Código_Ensayo/Ejercicios/vectores/main.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 
 int main(){
+    
     int vector[5]; /* ={1,5,54,0,998}; */
+    /* Variable de almacenamiento del vector */
     int i;
 
     for (int i=0;i<5;i++){
@@ -11,7 +13,7 @@ int main(){
     }
     for(i=0;i<5;i++){
         if(vector[i]==998){
-            printf("\n Posición de memoria del vector con el número 998 es: %d",i);
+            printf("\n Posición de memoria del vector con el número 998 es: %d \n",i);
         }
     }
     return 0;

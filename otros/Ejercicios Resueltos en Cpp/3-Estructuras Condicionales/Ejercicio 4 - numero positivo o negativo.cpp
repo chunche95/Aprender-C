03ea0a50d6a5164072733dfb5eb097181aa981ce
chunche95 <paeste95.pb@gmail.com>
2019-08-23 20:02:58 +0200

    Otros recursos de C+-

---
+++ otros/Ejercicios Resueltos en Cpp/3-Estructuras Condicionales/Ejercicio 4 - numero positivo o negativo.cpp
@@ -0,0 +1,28 @@
+/*
+ Comprobar si un número digitado por el usuario es positivo o negativo.
+ */
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int numero;
+	
+	cout<<"Digite un numero: "; 
+	cin>>numero;
+	
+	if(numero==0){
+		cout<<"El numero es cero";
+	}
+	else if(numero>0){
+		cout<<"El numero es positivo";
+	}
+	else{
+		cout<<"El numero es negativo";
+	}
+	
+	
+	
+	return 0;
+}
---
+++ otros/Ejercicios Resueltos en Cpp/3-Estructuras Condicionales/Ejercicio 5 - comprobando vocal.cpp
@@ -0,0 +1,26 @@
+/*
+ Escriba un programa que lea de la entrada estandar un caracter e indique 
+ en la salida estandar si el caracter es una vocal minuscula o no.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	char letra;
+	
+	cout<<"Digite un caracter: "; 
+	cin>>letra;
+	
+	switch(letra){
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u': cout<<"Es una vocal minuscula"; break;
+		default : cout<<"No es una vocal minuscula"; break;			
+	}
+		
+	return 0;
+}

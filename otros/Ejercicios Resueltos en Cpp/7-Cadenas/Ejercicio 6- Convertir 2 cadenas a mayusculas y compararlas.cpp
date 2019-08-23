2baaa49bda8db508bdea9b660d29ea95aad2a509
chunche95 <paeste95.pb@gmail.com>
2019-08-24 00:54:19 +0200

    Otros recursos de C+-

---
+++ otros/Ejercicios Resueltos en Cpp/7-Cadenas/Ejercicio 6- Convertir 2 cadenas a mayusculas y compararlas.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+
+using namespace std;
+
+int main(){
+	char palabra1[30],palabra2[30];
+	
+	cout<<"Digite una palabra: ";
+	cin.getline(palabra1,30,'\n');
+	cout<<"Digite una palabra: ";
+	cin.getline(palabra2,30,'\n');
+	
+	strupr(palabra1);
+	strupr(palabra2);
+	
+	if(strcmp(palabra1,palabra2)==0){
+		cout<<"\nAmbas cadenas son iguales";
+	}
+	else{
+		cout<<"Las cadenas no son iguales";
+	}
+	
+	getch();
+	return 0;
+}

c8382607df642a8b2e25775cce5cd35b464d1a15
chunche95 <paeste95.pb@gmail.com>
2019-08-24 00:25:09 +0200

    Otros recursos de C+-

--- otros/Ejercicios Resueltos en Cpp/7-Cadenas/Ejercicio 3 - Comparando dos cadenas de caracteres.cpp
+++ otros/Ejercicios Resueltos en Cpp/7-Cadenas/Ejercicio 3 - Comparando dos cadenas de caracteres.cpp
@@ -3,3 +3,29 @@
  * contrario, indicar cual es la mayor.
  */
 
+#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+    char p1[50], p2[50];
+
+    cout<<"Escriba la primera palabra: ";
+    cin.getline(p1,50,'\n');
+    cout<<"Escriba la segunda palabra: ";
+    cin.getline(p2,50,'\n');
+    
+    if(strcmp(p1,p2)==0){
+        cout<<"Ambas cadenas son iguales.";
+    }else if(strcmp(p1,p2)>0){
+        cout<<p1<<" Es mayor alfabeticamente.";
+    }else{
+        cout<<p2<<" Es mayor alfabeticamente.";
+    }
+
+    getch();
+    return 0;
+}
